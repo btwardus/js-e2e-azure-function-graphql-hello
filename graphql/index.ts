86c2077cbf060ae7c966c879894af8d0c142add0
Bo Twardus <btwardus@gmail.com>
2022-12-30 20:38:00 +0000

    schema changes

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,30 +1,92 @@
 import { ApolloServer, gql} from "apollo-server-azure-functions";
+import { v4 as uuid } from 'uuid';
 
-// Construct a schema, using GraphQL schema language
+const database = { [uuid()] :{"name": "John Doe", "dob": 1988-08-08, "address": "good morning"} };
+
 const typeDefs = gql`
-  type Query {
-    hello: String
-  }
+    input PersonInput {
+        name: String
+        dob: Date
+        address: String  
+    }
+
+    type Person {
+        id: ID!
+        name: String
+        address: String
+        
+    }
+
+    type Query {
+        getPerson(id: ID!): Person,
+        getPersons:[Person]
+    }
+
+    type Mutation {
+        createPerson(input: PersonInput): Person
+        updatePerson(id: ID!, input: PersonInput): Person
+    }
 `;
 
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => "Hello from our GraphQL backend!",
-  },
-};
+class Person {
 
+    id: any;
+    name: string;
+    dob: Date;
+    address: string;
+
+    constructor(id: String, {name, dob, address}) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.address = address;
+    }
+}
+
+const resolvers = {
+    Mutation: {
+        createPerson: (_, {input}) => {
+            const id = uuid();
+
+            database[id] = input;
+            return new Person(id, input);
+        },
+        updatePerson: (_, {id, input}) => {
+            if (!database[id]) {
+                throw new Error('no Person exists with id ' + id);
+            }
+            database[id] = input;
+            return new Person(id, input);
+        },
+    },
+    Query: {
+        getPerson: (_, {id}) => {
+            if (!database[id]) {
+                throw new Error('no Person exists with id ' + id);
+            }
+            return new Person(id, database[id]);
+        },
+        getPersons: (_, ) => {
+            let arr = [];
+            for (var key in database) {
+                if (database.hasOwnProperty(key)) {
+                    arr.push({
+                        id: key,
+                        name: database[key].name,
+                        dob: database[key].name,
+                        address: database[key].address
+                    });
+                }
+            }
+            return arr;
+        },
+    }
+};
 // @ts-ignore
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  debug: true
-});
+const server = new ApolloServer({ typeDefs, resolvers, playground: true});
 
 export default server.createHandler({
   cors: {
-    origin: ['*', "https://studio.apollographql.com"],
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["access-control-allow-credentials", "access-control-allow-origin", "content-type"]
+    origin: '*'
   },
 });
